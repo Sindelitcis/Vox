73eadc95c987cc73f52a2af63beaeacc6506c00a
Sindelitcis <rpm.socialgamers@gmail.com>
2021-09-11 16:50:14 -0300

    Arrumei embed Deslogar.

--- embeds/deslogar.js
+++ embeds/deslogar.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 const embed = (client, message) => new Discord.MessageEmbed()
     .setAuthor(`Escolha a opção desejada, ${message.author.username}.`, message.author.avatarURL())
-    .setColor(#f08315)
+    .setColor('#f08315')
     .setDescription(`\`⚒️ Lista de Personagens\`\n\`�💰����� CSaldo`\n\Inventáriosão.\`\n\`🎖️ Comandos variados.\`\n\`💵 Comandos de economia/level.\`.`)
     .setColor("#36393F")
 
